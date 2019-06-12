0cb404c62970b754f362610e1a54c0ccc36e863e
Jose Salcido <jmsalcidoaguilar@gmail.com>
2019-06-12 11:48:22 -0700

    preguntas del 12 de junio 💜

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ var questions = [
     '¿Hay algo que en realidad se pueda experimentar objetivamente?',
     '¿Tenemos libre albedrío?',
     '¿Por qué hay algo en vez de nada?',
+    '¿Te tienes que quejar?',
+    '¿Hoy por fin vas a ser mejor?',
 ];
 
 window.onload = function () {
